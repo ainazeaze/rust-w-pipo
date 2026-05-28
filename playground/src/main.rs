901b06d1d53e0ab0e4cebbd0e2f934ff731316aa
Aina Ratefiarivony <aina-tiavina.ratefiarivoni@buawei.com>
2026-05-28 13:46:17 +0200

    basics

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
     println!("Hello, world!");
-    let a = 'a'..'z';
+    let a = 'a';
+    println!("{a:#?}"); // debug print
+    dbg!(a); // debug print
+
+    // Option null does not exist
+    let a: Option<i32> = Some(3);
+    let b: Option<i32> = None;
+    // cant do a + 2 directly
+    let c = a.unwrap() + 2; // but not recommanded, crash in case a is None
+
+    // Exception does not exist, use Result<T,E>, use ? to get the value or return Err/None
+
+    // Collection
 }
