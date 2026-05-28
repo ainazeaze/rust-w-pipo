eb7ba67d832a6d54baecae113bc308d6f6a90cd3
Aina Ratefiarivony <aina-tiavina.ratefiarivoni@buawei.com>
2026-05-28 14:55:48 +0200

    petit refacto tu connais

--- aoc/src/bin/2015-1.rs
+++ aoc/src/bin/2015-1.rs
@@ -9,8 +9,8 @@ fn count_parenthesis(file_path: &str) -> io::Result<i32> {
 
     for c in data.chars() {
         match c {
-            '(' => count = count + 1,
-            ')' => count = count - 1,
+            '(' => count += 1,
+            ')' => count -= 1,
             _ => {}
         }
     }
