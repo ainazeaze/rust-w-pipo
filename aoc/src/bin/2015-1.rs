b7b5a57b6d7fc201986cdc2897f117ea3c057d8d
Aina Ratefiarivony <aina-tiavina.ratefiarivoni@buawei.com>
2026-05-28 14:53:42 +0200

    aoc 2015 day 1

---
+++ aoc/src/bin/2015-1.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+fn count_parenthesis(file_path: &str) -> io::Result<i32> {
+    let mut count: i32 = 0;
+    let mut f: File = File::open(file_path)?;
+    let mut data: String = String::new();
+    f.read_to_string(&mut data)?;
+
+    for c in data.chars() {
+        match c {
+            '(' => count = count + 1,
+            ')' => count = count - 1,
+            _ => {}
+        }
+    }
+    Ok(count)
+}
+
+fn main() -> io::Result<()> {
+    let count = count_parenthesis("input/2015-1.txt")?;
+    println!("{:}", count);
+    Ok(())
+}
---
+++ aoc/src/bin/2015-2.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+fn first_parenthesis_negative_floor(file_path: &str) -> io::Result<i32> {
+    let mut count: i32 = 0;
+    let mut index: i32 = 0;
+    let mut f: File = File::open(file_path)?;
+    let mut data: String = String::new();
+    f.read_to_string(&mut data)?;
+
+    for c in data.chars() {
+        if count == -1 {
+            return Ok(index);
+        }
+        index += 1;
+        match c {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => {}
+        }
+    }
+    Ok(index)
+}
+
+fn main() -> io::Result<()> {
+    let count = first_parenthesis_negative_floor("input/2015-1.txt")?;
+    println!("{:}", count);
+    Ok(())
+}
